d4e6cdc182a0872d07fb8e8c2d08cc34bf5a261a
JJBP55 <jonathanjbello.jb@gmail.com>
2022-09-09 13:35:50 -0400

    Basic statistics, Average with reduce

--- platzimath.js
+++ platzimath.js
@@ -1,14 +1,38 @@
 
 
-function calcularPromedio(lista){
-    //sumar todos los elementos y dividir la cantidad de elementos
-    let sumaLista = 0;
+// function calcularPromedio(lista){
+//     //sumar todos los elementos y dividir la cantidad de elementos
+//     let sumaLista = 0;
 
-    for (let i = 0; i < lista.length; i++){
-        sumaLista = sumaLista + lista[i];
-    }
+//     for (let i = 0; i < lista.length; i++){
+//         sumaLista = sumaLista + lista[i];
+//     }
+    
+//     const promedio = sumaLista / lista.length;
+//     console.log(promedio);
+//     return promedio;
+// }
+
+//metodo reduce
+
+function calcularPromedio(lista){
+
+    // function SumarElementos(valorAcumulado, nuevoValor){
+    //     return valorAcumulado + nuevoValor;
+    // }
+
+    // const sumarElementos =(valorAcumulado, nuevoValor) =>{
+    //     return valorAcumulado + nuevoValor;
+    // }
+    const sumarElementos =
+    (valorAcumulado, nuevoValor) => valorAcumulado + nuevoValor;
+
+    const sumaLista = lista.reduce(sumarElementos);
     
     const promedio = sumaLista / lista.length;
     console.log(promedio);
     return promedio;
 }
+
+//array functions
+//const a = () => {};
