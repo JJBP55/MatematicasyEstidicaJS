57a149bac264555497f433d7e68220152f25b752
JJBP55 <jonathanjbello.jb@gmail.com>
2022-10-05 19:35:58 -0400

    calculating median

--- platzimath.js
+++ platzimath.js
@@ -60,7 +60,19 @@ function calcularMediana(lista){
     const listaEsPar = esPar(lista);
 
     if (listaEsPar){
+        const indexMitad1ListaPar = (lista.length / 2) -1;
+        const indexMitad2ListaPar = lista.length / 2;
+        // const mitad1Listpar = lista[(lista.length / 2) -1];
+        // const mitad2Listpar = lista[lista.length / 2];
 
+        // lista[indexMitad1ListaPar]
+        // lista[indexMitad2ListaPar
+        // [lista[indexMitad1ListaPar], lista[indexMitad2ListaPar]]]
+        // const listaMitades = [mitad1Listpar, mitad2Listpar];
+        const listaMitades = [];
+        listaMitades.push(lista[indexMitad1ListaPar]);
+        listaMitades.push(lista[indexMitad2ListaPar]);
+        calcularPromedio(listaMitades)
     }
     else{
         const indexMitadListaImpar = Math.floor(lista.length / 2);
@@ -69,4 +81,15 @@ function calcularMediana(lista){
         console.log(medianaListaImpar);
         return medianaListaImpar;
     }
+}
+
+function calcularPromedio(lista){
+    function sumarTodosElementos(valorAcumulado, nuevoValor){
+        return valorAcumulado + nuevoValor; 
+    }
+
+    const sumaLista = lista.reduce(sumarTodosElementos);
+    const promedio = sumaLista / lista.length;
+    console.log(promedio);
+    return promedio;
 }
