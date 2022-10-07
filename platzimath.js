0c05bafd93b92cdea4d645561a4e6014ecf14f8c
JJBP55 <jonathanjbello.jb@gmail.com>
2022-10-07 17:48:23 -0400

    Moda 1

--- platzimath.js
+++ platzimath.js
@@ -56,6 +56,23 @@ function esImpar(lista){
     return lista.length % 2;  
 }
 
+function calcularModa(lista){
+    const listaCount = {}
+
+    for(let i=0; i < lista.length; i++){
+        const elemento = lista[i];
+
+        if(listaCount[elemento]){
+            listaCount[elemento] += 1;
+        }
+        else{
+            listaCount[elemento] = 1;
+        }
+    }
+
+    console.log(listaCount);
+}
+
 function calcularMediana(listaDesordenada){
     const lista = ordenarLista(listaDesordenada);
 
