5fc05a77c9984a9f2a3480bf288055f25d5347b4
JJBP55 <jonathanjbello.jb@gmail.com>
2022-10-09 10:15:54 -0400

    moda 2

--- platzimath.js
+++ platzimath.js
@@ -70,7 +70,14 @@ function calcularModa(lista){
         }
     }
 
-    console.log(listaCount);
+    const listaArray = Object.entries(listaCount);
+    const listaOrdenada = ordenarListaBidimensional(listaArray, 1);
+    const listaMaxNumber = listaOrdenada[listaOrdenada.length - 1];
+    const moda = listaMaxNumber[0];
+    // console.log({listaCount, listaArray, listaOrdenada, listaMaxNumber});
+    // console.log('La moda es: ' + listaMaxNumber[0]);
+
+    return moda;
 }
 
 function calcularMediana(listaDesordenada){
@@ -129,6 +136,18 @@ function ordenarLista(listaDesordenada){
 
     //const lista = listaDesordenada.sort(ordenarListaSort);
     const lista = listaDesordenada.sort((a,b) => a-b);
+
+    return lista;
+}
+
+// [ [0,1], [0,1], [0,1] ]
+function ordenarListaBidimensional(listaDesordenada){
+    function ordenarListaSort(valorAcumulado, nuevoValor){
+        return valorAcumulado[1] - nuevoValor[1];
+    }
+
+    //const lista = listaDesordenada.sort(ordenarListaSort);
+    const lista = listaDesordenada.sort(ordenarListaSort);
 
     return lista;
 }
